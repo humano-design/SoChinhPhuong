bfb3639fc7e806498e2f01af1b2a5349121bd13c
SPC <SPC@28-LAB402>
2025-03-14 09:19:47 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SoChinhPhuong
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            box = new TextBox();
+            ketqua = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(301, 250);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 65);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // box
+            // 
+            box.Location = new Point(164, 105);
+            box.Multiline = true;
+            box.Name = "box";
+            box.Size = new Size(120, 42);
+            box.TabIndex = 1;
+            // 
+            // ketqua
+            // 
+            ketqua.AutoSize = true;
+            ketqua.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ketqua.Location = new Point(502, 115);
+            ketqua.Name = "ketqua";
+            ketqua.Size = new Size(109, 32);
+            ketqua.TabIndex = 2;
+            ketqua.Text = "KẾT QUẢ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ketqua);
+            Controls.Add(box);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox box;
+        private Label ketqua;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿namespace SoChinhPhuong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int so=Convert.ToInt32(box.Text);
+            if (soChinhPhuong(so))
+            {
+                ketqua.Text = so + " là số chính phương";
+            }
+            else
+            {
+                ketqua.Text = so + " không phải số chính phương";
+            }
+        }
+        bool soChinhPhuong(int so) {
+            if (so < 0) return false;
+            int sqrt=(int)Math.Sqrt(so);
+            return sqrt * sqrt == so;
+        
+        }
+
+
+    }
+}
